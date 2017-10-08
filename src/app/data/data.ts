961daf473f49d515da529b2b7f5a3694d0f1d4a1
vdmakul <vdmakul@gmail.com>
2017-10-08 21:54:55 +0300

    sample data resources urls fix

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,18 +1,18 @@
 
 const travelSites: TravelSite[] = [
 {
-  'img': '/images/1.jpg',
+  'img': 'assets/images/1.jpg',
   'address': 'Sed perspiciatis',
   'phone': 1285968685,
   'weather': {
     'title': 'Et harum quidem',
-    'icon': '/images/cloudy.png',
+    'icon': 'assets/images/cloudy.png',
     'water': 20,
     'temperature': 30
   },
   'socialInfo': {
     'title': 'Nam libero voluptatem',
-    'img': '/images/b1.jpg',
+    'img': 'assets/images/b1.jpg',
     'followers': 2850,
     'following': 675,
   },
