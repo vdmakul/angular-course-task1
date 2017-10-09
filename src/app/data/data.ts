c5693a38cfa7593a6bc60bb69409dcd07e293666
vadim.akula <vadim.akula@corp.mail.ru>
2017-10-09 15:26:29 +0300

    list for site search component

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,22 +1,39 @@
-
-const travelSites: TravelSite[] = [
-{
-  'img': 'assets/images/1.jpg',
-  'address': 'Sed perspiciatis',
-  'phone': 1285968685,
-  'weather': {
-    'title': 'Et harum quidem',
-    'icon': 'assets/images/cloudy.png',
-    'water': 20,
-    'temperature': 30
+export const travelSites: TravelSite[] = [
+  {
+    'img': 'assets/images/1.jpg',
+    'address': 'Sed perspiciatis',
+    'phone': 1285968685,
+    'weather': {
+      'title': 'Et harum quidem',
+      'icon': 'assets/images/cloudy.png',
+      'water': 20,
+      'temperature': 30
+    },
+    'socialInfo': {
+      'title': 'Nam libero voluptatem',
+      'img': 'assets/images/b1.jpg',
+      'followers': 2850,
+      'following': 675,
+    },
+    'type': 'Lighthouse'
   },
-  'socialInfo': {
-    'title': 'Nam libero voluptatem',
-    'img': 'assets/images/b1.jpg',
-    'followers': 2850,
-    'following': 675,
-  },
-  'type': 'Hotel'
-}
+  {
+    'img': 'assets/images/2.jpg',
+    'address': 'Nam libero tempore',
+    'phone': 1285968685,
+    'weather': {
+      'title': 'Nemo enim ipsam',
+      'icon': 'assets/images/cloudy.png',
+      'water': 20,
+      'temperature': 30
+    },
+    'socialInfo': {
+      'title': 'Excepteur sint occaecat',
+      'img': 'assets/images/b2.jpg',
+      'followers': 2850,
+      'following': 675,
+    },
+    'type': 'Lighthouse'
+  }
 
 ];
--- src/app/site-search/site-search.component.ts
+++ src/app/site-search/site-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { travelSites } from '../data/data';
 
 @Component({
   selector: 'app-site-search',
@@ -7,28 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteSearchComponent implements OnInit {
 
-  public travelSite: TravelSite = {
-    'img': 'assets/images/1.jpg',
-    'address': 'Sed perspiciatis',
-    'phone': 1285968685,
-    'weather': {
-      'title': 'Et harum quidem',
-      'icon': 'assets/images/cloudy.png',
-      'water': 20,
-      'temperature': 30
-    },
-    'socialInfo': {
-      'title': 'Nam libero voluptatem',
-      'img': 'assets/images/b1.jpg',
-      'followers': 2850,
-      'following': 675,
-    },
-    'type': 'Hotel'
-  };
+
+  public travelSites: TravelSite[] = travelSites;
+
+  public selectedSite: TravelSite;
+
+  public siteClick(site: TravelSite): void {
+    this.selectedSite = site;
+  }
 
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.selectedSite = this.travelSites[0];
   }
 
 }
